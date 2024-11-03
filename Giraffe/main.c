1557f34525af42ac0a662007838fc0b86abecf06
quackers71 <robq71@gmail.com>
2024-11-03 19:10:43 +0000

    working with numbers

--- Giraffe/main.c
+++ Giraffe/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int main()
 {
@@ -20,6 +21,10 @@ int main()
     char grade = 'A';
     char phrase[] = "Hello World!";
 
+    double desi = 15.984;
+    double a = 5.0;
+    double b = 4.5;
+    double result = a * b;
     // printf Function
     printf("\"Hello, \nWorld\"\n");
 
@@ -27,6 +32,22 @@ int main()
     printf("My favourite %s is %d\n", wordy, myNum);
     printf("gpa = %f\n", gpa);
     printf("My fave char is %c\n", q);
+
+    // Working with Numbers
+    printf("%f\n", desi);
+    printf("%.2f * %.2f = %.2f\n", a, b, result);
+
+    /* double num;
+    num = 36.356;
+    double ceilResult = ceil(num); */
+
+    printf("2^3 = %.2f\n", pow(2, 3)); // 2^3
+    printf("4^3 = %.2f\n", pow(4, 3) ); // 4^3
+    printf("Square root of 100 is %.0f\n", sqrt(100) );
+    // printf("ceiling of %f is %f\n", num, ceilResult); // don't work in CodeBlocks :-( ?!!!
+
+    printf("ceiling of 36.356 is %.2f\n", ceil(36.356));
+    printf("floor of 36.356 is %.2f\n", floor(36.356) );
 
     return 0;
 }
