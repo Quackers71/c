d91e77aa84ec9824c0cb2db1a310a5b0c7e3375e
quackers71 <robq71@gmail.com>
2024-11-03 09:39:03 +0000

    variables

---
+++ Giraffe/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char charName[] = "Russ";
+    int charAge = 35;
+
+    printf("There was a man named %s\n", charName);
+    printf("he was %d years old.\n", charAge);
+    printf("He really like the name %s\n", charName);
+    printf("but did't like being %d.\n", charAge);
+
+    return 0;
+}
