9169d8890caa11196acbcb09c9844bd45e5ffc59
quackers71 <robq71@gmail.com>
2024-11-03 20:49:20 +0000

    Constants

--- Giraffe/main.c
+++ Giraffe/main.c
@@ -4,6 +4,17 @@
 
 int main()
 {
+    // Constants
+
+    const int NUM = 5;
+    printf("Your constant number is %d\n", NUM);
+    // num = 8;
+    // printf("%d\n", num);
+
+    return 0;
+/* 
+    // printf stuff
+
     char charName[] = "Russ";
     int charAge = 35;
     int myNum = 7;
@@ -34,12 +45,13 @@ int main()
     printf("My fave char is %c\n", q);
 
     // Working with Numbers
+
     printf("%f\n", desi);
     printf("%.2f * %.2f = %.2f\n", a, b, result);
 
     /* double num;
     num = 36.356;
-    double ceilResult = ceil(num); */
+    double ceilResult = ceil(num); 
 
     printf("2^3 = %.2f\n", pow(2, 3)); // 2^3
     printf("4^3 = %.2f\n", pow(4, 3) ); // 4^3
@@ -48,6 +60,5 @@ int main()
 
     printf("ceiling of 36.356 is %.2f\n", ceil(36.356));
     printf("floor of 36.356 is %.2f\n", floor(36.356) );
-
-    return 0;
+    */
 }
