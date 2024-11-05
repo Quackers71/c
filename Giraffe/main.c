1f3f01825de2993cd14eca54cbfe097d2da2b7aa
quackers71 <robq71@gmail.com>
2024-11-05 21:51:27 +0000

    Mad Libs inputs complete

--- Giraffe/main.c
+++ Giraffe/main.c
@@ -7,9 +7,20 @@ int main()
 {
     // Mad Libs
 
-    printf("Roses are red\n");
-    printf("Violets are blue\n");
-    printf("I love you\n");
+    char color[20];
+    char pluralNoun[20];
+    char celebrity[20];
+
+    printf("Please enter a color : ");
+    scanf("%s", color);
+    printf("Please enter a plural noun : ");
+    scanf("%s", pluralNoun);
+    printf("Please enter a celebrity : ");
+    scanf("%s", celebrity);
+
+    printf("Roses are %s\n", color);
+    printf("%s are blue\n", pluralNoun);
+    printf("I love %s\n",celebrity);
 
     return 0;
 /*
