bd555732ac4410c526ffff1979fd83a4f34a21a8
Rob Q <robq@Robs-MacBook-Pro.local>
2025-09-26 10:06:13 +0100

    Strings

---
+++ C-MemMgmt/strings/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    char *will_never_hear_again = "Hey Q, when is the memory course in C gonna be done?";
+
+    // multi variable declaration
+    char* p1 = "Yes!", *p2 = "No!";
+    char *q1, q2;
+
+    // don't touch this line below
+    printf("%s\n", will_never_hear_again);
+    return 0;
+}
