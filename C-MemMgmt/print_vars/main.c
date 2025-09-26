f876a1e139546ab97ee209f080642bbb09ac9ee9
Rob Q <robq@Robs-MacBook-Pro.local>
2025-09-26 10:20:37 +0100

    updated print vars

--- C-MemMgmt/print_vars/main.c
+++ C-MemMgmt/print_vars/main.c
@@ -7,9 +7,9 @@ int main() {
     char *sneklang_title = "Sneklang";
 
     // don't touch above this line
-    printf("Sneklang default max threads : %d\n", sneklang_default_max_threads);
-    printf("Sneklang default perms : %c\n", sneklang_default_perms);
-    printf("Sneklang default PI : %f\n", sneklang_default_pi);
-    printf("Sneklang Title : %s\n", sneklang_title);
+    printf("Default max threads : %d\n", sneklang_default_max_threads);
+    printf("Default perms : %c\n", sneklang_default_perms);
+    printf("Default PI : %f\n", sneklang_default_pi);
+    printf("Title : %s\n", sneklang_title);
     return 0;
 }
