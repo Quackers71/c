944b97deaa6721a55a3493f31b7cff13554a1c32
Quackerz <robq71@gmail.com>
2025-10-05 18:40:37 +0100

    re-configured memaddr including Outputs

--- C-MemMgmt/memaddr/Output.md
+++ C-MemMgmt/memaddr/Output.md
@@ -1,6 +1,8 @@
-## Output 
+## Output
+
+memaddr_old (renamed)
 ```
-% ./memaddr 
+% ./memaddr_old
 Running suite: colors
 
 Assertion Failed in test 'munit_test_func_size_of_addr_16': size should be 4
@@ -23,8 +25,30 @@ Passed: 0, Failed: 4
 0 of 4 (0%) tests successful
 ```
 
-memaddr TBC
+memaddr
 ```
 % ./memaddr
-# TBC
+Running suite: colors
+[SUCCESS] Test: /size_of_addr_16
+[SUCCESS] Test: /size_of_addr_32
+[SUCCESS] Test: /size_of_addr_64
+[SUCCESS] Test: /size_of_addr_128
+--- Test Summary ---
+Passed: 4, Failed: 0
+4 of 4 (100%) tests successful
+```
+
+memfulladdr
+```
+% ./memfulladdr                                   
+The variable 'i' holds the value: 128
+The memory address of 'i' is: 0x16f542cd8
+Running suite: colors
+[SUCCESS] Test: /size_of_addr_16
+[SUCCESS] Test: /size_of_addr_32
+[SUCCESS] Test: /size_of_addr_64
+[SUCCESS] Test: /size_of_addr_128
+--- Test Summary ---
+Passed: 4, Failed: 0
+4 of 4 (100%) tests successful
 ```
--- C-MemMgmt/memaddr/main.c
+++ C-MemMgmt/memaddr/main.c
@@ -6,25 +6,30 @@
 
 munit_case(RUN, size_of_addr_16, {
     unsigned long size = size_of_addr(16);
-    munit_assert_ulong(size, ==, 4, "size should be 4");
+    munit_assert_ulong(size, ==, 8, "size should be 8 on a 64-bit system");
 });
 
 munit_case(RUN, size_of_addr_32, {
     unsigned long size = size_of_addr(32);
-    munit_assert_ulong(size, ==, 4, "size should be 4");
+    munit_assert_ulong(size, ==, 8, "size should be 8 on a 64-bit system");
 });
 
 munit_case(RUN, size_of_addr_64, {
     unsigned long size = size_of_addr(64);
-    munit_assert_ulong(size, ==, 4, "size should be 4");
+    munit_assert_ulong(size, ==, 8, "size should be 8 on a 64-bit system");
 });
 
 munit_case(RUN, size_of_addr_128, {
     unsigned long size = size_of_addr(128);
-    munit_assert_ulong(size, ==, 4, "size should be 4");
+    munit_assert_ulong(size, ==, 8, "size should be 8 on a 64-bit system");
 });
 
 int main() {
+
+    long long i = 128;
+    printf("The variable 'i' holds the value: %lld\n", i);
+    printf("The memory address of 'i' is: %p\n", (void *)&i);
+
     MunitTest tests[] = {
         munit_test("/size_of_addr_16", size_of_addr_16),
         munit_test("/size_of_addr_32", size_of_addr_32),
