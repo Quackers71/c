4caa9cf83bf8d90d79fb674095be9345ae81a893
Quackerz <robq71@gmail.com>
2025-10-05 14:54:26 +0100

    memory address checking

---
+++ C-MemMgmt/memaddr/snek.c
@@ -0,0 +1,4 @@
+unsigned long size_of_addr(long long i) {
+    unsigned long size_of_snek_version = sizeof(i);
+    return size_of_snek_version;
+}
---
+++ C-MemMgmt/memaddr/snek.h
@@ -0,0 +1,7 @@
+#ifndef SNEK_H
+#define SNEK_H
+
+// Declare the prototype for the size_of_addr function
+unsigned long size_of_addr(long long i);
+
+#endif // SNEK_H
