eedc21cc6df53ff1766870c3df4624f2a7e2040d
Quackerz <robq71@gmail.com>
2025-10-08 20:47:01 +0100

    added some printf output of addresses and values

--- C-MemMgmt/coordinate/Output.md
+++ C-MemMgmt/coordinate/Output.md
@@ -11,3 +11,25 @@ Running suite: pointers
 Passed: 2, Failed: 0
 2 of 2 (100%) tests successful
 ```
+
+with some printf outputs
+```
+% ./coordinate 
+
+coordinate_update_x            = 0x1008bc7bc
+coordinate_update_and_return_x = 0x1008bc828
+new_coordinate                 = 0x1008bc728
+
+Running suite: pointers
+test unchanged old.x = 1
+test unchanged old.x = 1
+[SUCCESS] Test: /test_unchanged
+
+test changed   new.x = 4
+test changed   old.x = 1
+[SUCCESS] Test: /test_changed
+
+--- Test Summary ---
+Passed: 2, Failed: 0
+2 of 2 (100%) tests successful
+```
--- C-MemMgmt/coordinate/main.c
+++ C-MemMgmt/coordinate/main.c
@@ -6,9 +6,12 @@
 munit_case(RUN, test_unchanged, {
     coordinate_t old = new_coordinate(1, 2, 3);
     munit_assert_int(old.x, ==, 1, "old.x must be 1");
+    printf("test unchanged old.x = %d\n", old.x);
 
     coordinate_update_x(old, 4);
     munit_assert_int(old.x, ==, 1, "old.x must be 1");
+    printf("test unchanged old.x = %d\n", old.x);
+
 });
 
 munit_case(RUN, test_changed, {
@@ -17,15 +20,24 @@ munit_case(RUN, test_changed, {
 
     coordinate_t new = coordinate_update_and_return_x(old, 4);
     munit_assert_int(new.x, ==, 4, "new.x must be 4");
+    printf("test changed   new.x = %d\n", new.x);
+
     munit_assert_int(old.x, ==, 1, "old.x must be 1");
+    printf("test changed   old.x = %d\n", old.x);
 
     munit_assert_ptr_not_equal(&old, &new, "Must not be the same address");
 });
 
 int main() {
+
+    printf("\ncoordinate_update_x            = %p\n", coordinate_update_x);
+    printf("coordinate_update_and_return_x = %p\n", coordinate_update_and_return_x);
+    printf("new_coordinate                 = %p\n", new_coordinate);
+    printf("\n");
+    
     MunitTest tests[] = {
-        munit_test("/test_unchanged", test_unchanged),
-        munit_test("/test_changed", test_changed),
+        munit_test("/test_unchanged\n", test_unchanged),
+        munit_test("/test_changed\n", test_changed),
         munit_null_test,
     };
 
