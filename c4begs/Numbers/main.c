9c4ff19624c2d65b38ce77fb70b71f793622a239
quackers71 <robq71@gmail.com>
2025-10-19 18:53:54 +0100

    working with numbers

---
+++ c4begs/Numbers/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    int num = 6;
+    printf("%d\n", num);
+
+    printf("%.f\n", pow(4, 3));
+    printf("%.f\n", sqrt(81));
+    printf("%.2f\n", ceil(36.356));
+    printf("%.2f\n", floor(36.656));
+
+    printf("%.2f\n", 8.9);
+    printf("%.2f\n", 5 * 4.5);
+
+    return 0;
+}
