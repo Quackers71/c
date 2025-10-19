bd493d5556befca58851fa8db87331d47aac907d
quackers71 <robq71@gmail.com>
2025-10-19 20:08:38 +0100

    Constants

---
+++ c4begs/Constants/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    const int NUM = 5;
+    printf("CONST NUM = %d\n",NUM);
+
+    printf("Hello\n"); // this is a constant
+
+    printf("%d\n", 85); // this is also a constant
+
+    return 0;
+}
