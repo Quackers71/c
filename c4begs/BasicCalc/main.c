190cadace5e8c41d86f048b5372a144cf941194d
quackers71 <robq71@gmail.com>
2025-10-20 23:16:43 +0100

    updated Calc

--- c4begs/BasicCalc/main.c
+++ c4begs/BasicCalc/main.c
@@ -3,17 +3,17 @@
 
 int main()
 {
-    char operator;
     double num1, num2;
-
-    printf("Please enter your operator (+, -, * or /) : ");
-    scanf("%c", &operator);
+    char operator;
 
     printf("Enter your first number : ");
     scanf("%lf", &num1);
 
     printf("Enter your second number : ");
     scanf("%lf", &num2);
+
+    printf("Please enter your operator (+, -, * or /) : ");
+    scanf(" %c", &operator);
 
     switch (operator) {
         case '+':
@@ -26,7 +26,7 @@ int main()
             printf("%.2lf * %.2lf = %.2lf\n", num1, num2, num1 * num2);
             break;
         case '/':
-            if (num1 && num2 != 0) {
+            if (num2 != 0) {
                 printf("%.2lf / %.2lf = %.2lf\n", num1, num2, num1 / num2);
             } else {
                 printf("Error: Division by zero is not allowed.\n");
