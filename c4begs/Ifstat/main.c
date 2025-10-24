b53d42ae0a1969ebb23cf88333d2881bbdd3e175
Quackerz <robq71@gmail.com>
2025-10-24 09:38:39 +0100

    using if statements & returning values from functions

---
+++ c4begs/Ifstat/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int max(int num1, int num2, int num3) {
+    int result;
+    if (num1 >= num2 && num1 >= num3) {
+        result = num1;
+    } else if (num2 >= num1 && num2 >= num3) {
+        result = num2;
+    } else {
+        result = num3;
+    }
+    return result;
+}
+
+const char *trueOrFalse(int num1, int num2, int num3) {
+    int result;
+    if (num1 < num2 || num2 > num3) {
+        return "True";
+    } else {
+        return "False";
+    }
+}
+
+int main() {
+
+    printf("%d\n", max(45, 71, 3));
+    printf("%s\n", trueOrFalse(3, 2, 5));
+
+    return 0;
+}
