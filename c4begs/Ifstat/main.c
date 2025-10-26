62cd1484661d4a21ede7d4e008582a47f5bff7ec
Quackerz <robq71@gmail.com>
2025-10-26 10:23:00 +0000

    changed function return statement into a ternary operator

--- c4begs/Ifstat/main.c
+++ c4begs/Ifstat/main.c
@@ -13,21 +13,18 @@ int max(int num1, int num2, int num3) {
     return result;
 }
 
-// https://share.google/aimode/ugLWISCDtqmVjaRia < This explains what I missed when creating this function
+// https://share.google/aimode/sQTNHYbnOqgWXIb0p < This explains what I missed when creating this function
 /* It also provides another method to return an integer to represent boolean logic, where 1 is true and 0 is false.
    This avoids complex memory management and is safer */ 
 
-const char *trueOrFalse(int num1, int num2, int num3) {
-    return (num1 < num2 || num2 > num3)? "True" : "False";
+int trueOrFalse(int num1, int num2, int num3) {
+    return (num1 < num2 || num2 > num3)? 1 : 0;
 }
 
 int main() {
 
     printf("%d\n", max(45, 71, 3));
-
-    // Ternary operator is used to print the string based on the integer result
-    // This has been changed back for now!
-    printf("%s\n", trueOrFalse(3, 2, 5));
+    printf("%s\n", trueOrFalse(3, 2, 5) ? "True" : "False");
 
     return 0;
 }
