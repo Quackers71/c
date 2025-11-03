22de2cb88801a058e9a7fca66033f3c0c9fbc0f2
Quackerz <robq71@gmail.com>
2025-11-03 19:12:26 +0000

    Function, called by value

--- Coop/FuncCall/main.c
+++ Coop/FuncCall/main.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+int mymult(int a, int b) 
+    // int a, b; declare in the function
+{
+    int c = a * b;
+    return c;
+}
 
-    
+int main() {
+    // int mymult(); not require as retval calls the function declaration
+    int retval;
+
+    retval = mymult(6, 7);
+    printf("Answer : %d\n", retval);    
     
     return 0;
 }
