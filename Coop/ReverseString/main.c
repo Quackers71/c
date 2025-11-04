43bb08504ac09ec50e4ca38c7018369454a09cfb
Quackerz <robq71@gmail.com>
2025-11-04 21:58:50 +0000

    tidied up and added some comments

--- Coop/ReverseString/main.c
+++ Coop/ReverseString/main.c
@@ -4,11 +4,12 @@
 
 // Suggested improvements from G-AI mode : https://share.google/aimode/JOItgvRiKhtvVmyYJ
 
+// There is a strlen function provided by string.h
 int str_len(char self[]) {
 
     int i;
     for (i = 0; self[i]; i++);
-    return i - 1;
+    return i - 1; // removes the newline character add by fgets
 }
 
 int main() {
@@ -17,25 +18,16 @@ int main() {
     printf("Enter a string : ");
     fgets(str2rev, 100, stdin);
 
-    // printf("You entered    : %s", str2rev); //, str_len(str2rev));
-
-    // char last_char_value;
     int total_string_length;
 
+    // retreives the total length of the string
     for (int i = 0; i < str_len(str2rev); i++) {
-        // printf("%d : %c\n", i, str2rev[i]);
-
-        // last_char_value = str2rev[i];
         total_string_length = i;
     }
-
-    // printf("\nThe last char is '%c'\n", last_char_value);
-    
-    // int last_value = last_char_value;
-    // printf("%d\n", total_string_length);
  
     printf("In reverse     : ");
 
+    // starts at the last char and reverse prints out each char in the char array 
     for (int x = total_string_length; x >= 0; x--) {
         printf("%c", str2rev[x]);
     }
