69aa8cf377d471e9e2866b69fa8ed0ba8f911963
Quackerz <robq71@gmail.com>
2025-11-05 23:17:03 +0000

    counting lines, words & chars from input using while, if, else if & define for True - False flags

---
+++ Coop/WordCount/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define YES 1
+#define NO 0
+
+int main() {
+
+    /* count lines, words, chars in input */
+    
+    int chars, numline, numword, numchar, inword;
+
+    inword = NO;
+    numline = numword = numchar = 0;
+    while ((chars = getchar()) != EOF) {
+        ++numchar;
+        if (chars == '\n')
+            ++numline;
+        if (chars == ' ' || chars == '\n' || chars == '\t')
+            inword = NO;
+        else if ( inword == NO ) {
+            inword = YES;
+            ++numword;
+        }
+    }
+
+    printf("%d line(s), %d word(s) %d character(s) including spaces\n", numline, numword, numchar -1);
+
+    return 0;
+}
