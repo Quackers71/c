dca77c85b077c230bb877ea136b4fd2d0ef77627
Quackerz <robq71@gmail.com>
2025-11-08 21:52:32 +0000

    constant variables including scopes

--- PortfolioC/Constants/main.c
+++ PortfolioC/Constants/main.c
@@ -1,20 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define LENGTH 10 // using the define pre-processor statement
+// #define LENGTH 10 // using the define pre-processor statement
 
-void print_array(int *array);
+void print_array(int *array, int size);
+
+const int len = 5; // This is a compile time constant and can be used in any function including main
 
 int main() {
 
-    int a[LENGTH] = {0,1,2,3,4,5,6,7,8,9,};
-    printf("array length : %d\n", LENGTH);
-    print_array(a);
+    int a[] = {0,1,2,3,4,5,6,7,8,9,};
+    print_array(a, 10);
+
+    printf("\n");
+    int b[] = {0,1,2,3,4,5,6,7,8,9,10,11,12};
+    print_array(b, 12);
+
+    printf("len: %d\n", len);
 
     return 0;
 }
 
-void print_array(int *array) {
-    for (int i = 0; i < LENGTH; i++)
+void print_array(int *array, int size) {
+    const int length = size; // Constant variable, only exists (scope) within the function
+    for (int i = 0; i < length; i++)
         printf("array[%d]=%d\n",i , array[i]);
+
+    printf("len: %d\n", len);
 }
