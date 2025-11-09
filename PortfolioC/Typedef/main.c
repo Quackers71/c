6b56e24e370eeac1086831adadc139b1a3bb4f65
Quackerz <robq71@gmail.com>
2025-11-09 10:44:40 +0000

    using typedef

---
+++ PortfolioC/Typedef/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int kilometer;
+
+int main() {
+
+    kilometer newyork, chicago, miami;
+
+    newyork = 300;
+    chicago = 500;
+    miami = 1900;
+
+    printf("new york : %d\n", newyork);
+    printf("chicago  : %d\n", chicago);
+    printf("miami    : %d\n", miami);
+    
+    return 0;
+}
