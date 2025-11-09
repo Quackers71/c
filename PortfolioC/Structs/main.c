b76acab093859aa63c9f2547b044f876060928da
Quackerz <robq71@gmail.com>
2025-11-09 23:37:06 +0000

    refined, went over the code & added comments

--- PortfolioC/Structs/main.c
+++ PortfolioC/Structs/main.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+/* Ask AI to explain through & I read through this explanation - https://share.google/aimode/p5o4Xgmjgey5pJwUx */
+
 // Student:
 //   - name (string)
 //   - id (string)
@@ -34,22 +36,31 @@ int main() {
 
     Student kevin;
     
-    strcpy(kevin.name, "Kevin");
-    strcpy(kevin.id, "000123123");
-    kevin.age =40;
+    strcpy(kevin.name, "Kevin"); // strcpy is used to populate the string literal
+    strcpy(kevin.id, "000123123"); // as C strings cannot be assigned directly using the = operator
+    kevin.age = 40;
     kevin.grades[0] = 1;
     kevin.grades[1] = 2;
     kevin.grades[2] = 3;
     kevin.grades[3] = 4;
     kevin.grades[4] = 5;
 
+    Student george;
+
+    strcpy(george.name, "George");
+    strcpy(george.id, "000123128");
+    george.age = 21;
+    george.grades[0] = 1;
+    george.grades[1] = 2;
+    george.grades[2] = 3;
+    george.grades[3] = 4;
+    george.grades[4] = 5;
+
     print_student(kevin);
+    print_student(george);
 
     Point p1 = {5, 10};
-    Point p2 = {
-        .y = 8, 
-        .x = 2
-    };
+    Point p2 = {.y = 8, .x = 2}; // specific member initialisers, so x & y can be place in any order
 
     printf("p1.x: %d\n", p1.x);
     printf("p1.y: %d\n", p1.y);
@@ -57,9 +68,9 @@ int main() {
     printf("p2.x: %d\n", p2.x);
     printf("p2.y: %d\n", p2.y);
 
-    p1 = p2; // a memory copy
+    p1 = p2; // a memory copy or member-wise copy
 
-    printf("p1 = p2\n");
+    printf("*** p1 = p2 *** // memory copy\n");
     printf("p1.x: %d\n", p1.x);
     printf("p1.y: %d\n", p1.y);
 
@@ -72,10 +83,10 @@ int main() {
 
     print_points(points);
 
-    Data x;
+    Data x; // Data variables declared
     Data y;
 
-    x.array = malloc(sizeof(int) * 5);
+    x.array = malloc(sizeof(int) * 5); // memory dynamically allocated on the heap 
     y.array = malloc(sizeof(int) * 5);
 
     x.array[0] = 1;
@@ -90,17 +101,18 @@ int main() {
     y.array[3] = 9;
     y.array[4] = 9;
 
-    x = y; // memory copy of the pointer not the array! (or what the struct is storing)
+    y = x; // memory copy (or shallow copy) of the pointer, not the values in the array! (or what the struct is storing)
+    printf("y = x // y is now set to the pointer of the x *array\n");
 
     for (int i = 0; i < 5; i++)
-        printf("x.array[%d] = %d\n", i, x.array[i]);
+        printf("x.array[%d] = %d and memory address is : %p\n", i, x.array[i], &x.array[i]);
 
-    x.array[0] = 10;
+    x.array[0] = 8;
 
-    printf("modified x.array[0] = 10\n"); // modified the x.array[0]
+    printf("*** x.array[0] = 8 *** // this is modified\n"); // modified the x.array[0]
 
     for (int i = 0; i < 5; i++)
-        printf("y.array[%d] = %d\n", i, y.array[i]); // You will see that y was modified
+        printf("y.array[%d] = %d and memory address is : %p\n", i, y.array[i], &y.array[i]); // You will see that y was modified
     
     return 0;
 }
