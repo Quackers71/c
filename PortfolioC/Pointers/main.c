0013da6c2816149028eb0c617bc0e0e16c16d5e7
Quackerz <robq71@gmail.com>
2025-11-11 22:09:15 +0000

    further clarification of pass by pointers

--- PortfolioC/Pointers/main.c
+++ PortfolioC/Pointers/main.c
@@ -1,14 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void swap(int *a, int *b);
+
 int main() {
 
-    int a, b, c;
+    int x, y;
+    x = 5;
+    y = 10;
+
+    printf("x: %d   y: %d\n", x, y);
+    printf("&x : %p\n&y : %p\n", &x, &y);
+    swap(&x, &y);
+
+    printf("x: %d  y: %d\n", x, y);
+    printf("&x : %p\n&y : %p\n", &x, &y);
+
+    /* int a, b, c;
     a = b = c = 0;
 
     printf("Enter 3 numbers : ");
     scanf("%d %d %d", &a, &b, &c); // Pass by Reference or Pass by Pointer in C
-    printf("Result : %d\n", a + b + c);
+    printf("Result : %d\n", a + b + c); */
     
     return 0;
+}
+
+void swap(int *a, int *b) {
+
+    printf("a  : %p\nb  : %p\n", a, b);
+    printf("*a : %d\n*b : %d\n", *a, *b);
+
+    int temp = 0;
+    temp = *a;
+    *a = *b;
+    *b = temp;
 }
