d3e34bd2999d2563acd695bb8b56f0128e4f976c
Quackers <q@quackers.com>
2025-11-24 17:07:25 +0000

    fixed the int with long, but revisit this stuff

--- PortfolioC/ReallocSecVuln/main.c
+++ PortfolioC/ReallocSecVuln/main.c
@@ -12,17 +12,40 @@ int main() {
 
     char *password1;
     char *password2;
+    char *other_ptr;
 
     password1 = malloc(7 * sizeof(char));
     password2 = malloc(7 * sizeof(char));
     strcpy(password1, "abc123");
     strcpy(password2, "xyz789");
 
-    printf("p1 : %p\np2 : %p\n", password1, password2);
-    printf("p1 : %d\np2 : %d\n", (int) password1, (int) password2);
+    printf("p1 addr  : %p\np2 addr  : %p\n", password1, password2);
+    printf("p1 (int) : %ld\np2 (int) : %ld\n",(long) password1, (long) password2);
 
     printf("\n");
 
+    for (int i = 0; i < 100; i++)
+        printf("%c", password1[i]);
+    printf("\n\n");
+
+    other_ptr = password1;
+    printf("other ptr copied from p1 : %s\n\n", other_ptr);
+
+    password1 = realloc(password1, 20 * sizeof(char));
+
+    printf("p1 addr  : %p\np2 addr  : %p\n", password1, password2);
+    printf("p1 (int) : %ld\np2 (int) : %ld\n",(long) password1, (long) password2);
+
+    printf("p1 : %s\n\n", password1);
+
+    printf("other ptr addr : %ld\n", (long) other_ptr);
+
+    printf("other ptr str  : %s\n", other_ptr); // supposed to print copy of password1 but doesn't?!!
+
+    for (int i = 0; i < 100; i++)
+        printf("%c", other_ptr[i]);
+    printf("\n\n");
+    
     free(password1);
     free(password2);
     
