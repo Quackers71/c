5892d5fbc938cfbc3abcfc4687fd430fb240eb7f
Quackerz <robq71@gmail.com>
2025-12-02 21:21:15 +0000

    fixed with malloc(strlen() & strcpy() methods

--- PortfolioC/HeapInsp/main.c
+++ PortfolioC/HeapInsp/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main() {
 
@@ -11,21 +12,38 @@ int main() {
     printf("Please enter your password : ");
     scanf("%s", password);
 
-    other_ptr = password;
-    printf("password  : %s\n", password);
-    printf("other_ptr : %s\n", other_ptr);
+    // other_ptr = password;
+
+    other_ptr = malloc((strlen(password) + 1) * sizeof(char));
+    strcpy(other_ptr, password);
+    printf("\nallocated memory & strcpy 'password' into 'other_ptr'\n");
+
+    printf("\n'password'          : %s\n", password);
+    printf("'password' address  : %p\n", password);
+
+    printf("\n'other_ptr'         : %s\n", other_ptr);
+    printf("'other_ptr' address : %p\n", other_ptr);
+
+    printf("\n'password' chars array output : ");
+    for (int i = 0; i < 10; i++)
+        printf("%c", password[i]);
+    printf("\n");
 
     free(password);
-    printf("\nfree(password) executed\n");
+    printf("\nfreed 'password' memory\n");
 
-    printf("password  : %s\n", password);
-    printf("other_ptr : %s\n", other_ptr);
-
-    // in the demo this printed out the string of chars entered by the User
-    printf("other_ptr chars : ");
+    printf("\n'other_ptr' chars array output : ");
     for (int i = 0; i < 10; i++)
         printf("%c", other_ptr[i]);
     printf("\n");
-            
+
+    printf("\n'password'          : %s\n", password);
+    printf("'password' address  : %p\n", password);
+
+    printf("\n'other_ptr'         : %s\n", other_ptr);
+    printf("'other_ptr' address : %p\n", other_ptr);
+
+    free(other_ptr);
+          
     return 0;
 }
